f05902c5c05688a035193238871e73d09bb7f5f2
Suraj Kumar Jana <surajjana2@gmail.com>
2016-03-29 10:55:49 +0530

    new commit

--- scrape.py
+++ scrape.py
@@ -2,31 +2,32 @@ import re
 import json
 from subprocess import call
 
-json_data = '{"hospital_names":['
+#json_data = '{"hospital_names":['
 
-for j in range(7,12):
-	print j
-	call("curl 'https://www.hospitalkhoj.com/hospitals/bangalore/page-"+str(j)+"' > test1.html", shell=True)
+fd = open("data1.json", "a")
 
-	fo = open("test1.html", "r")
-	data = fo.read()
+# for j in range(90,91):
+# 	print j
+# 	call("curl 'http://hospitalkhoj.com/index.php?option=com_directory&view=listings&city_id=40' > test1.html", shell=True)
 
-	for i in range(1,16):
-		#print i
-		rxp1 = re.findall(r'<ul id="college-list">(.*?)</ul>',data,re.DOTALL)
-		res_val = rxp1[0].split('<li  >')
-		rxp2 = re.findall(r'<div class="cl-list-name">(.*?)</div>',res_val[i],re.DOTALL)
-		#print rxp2
-		rxp3 = re.findall(r''+str(">\n")+'(.*?)</a>',rxp2[0],re.DOTALL)
-		#print rxp3
-		res_val = rxp3[0].split("\t")
+fo = open("test2.html", "r")
+data = fo.read()
 
-		# print i,". ",res_val[6]
-		json_data += '{"name":"'+res_val[6]+'"},'
-json_data += ']}'
+for i in range(1,16):
+	#print i
+	rxp1 = re.findall(r'<ul id="college-list">(.*?)</ul>',data,re.DOTALL)
+	res_val = rxp1[0].split('<li  >')
+	rxp2 = re.findall(r'<div class="cl-list-name">(.*?)</div>',res_val[i],re.DOTALL)
+	#print rxp2
+	rxp3 = re.findall(r''+str(">\n")+'(.*?)</a>',rxp2[0],re.DOTALL)
+	#print rxp3
+	res_val = rxp3[0].split("\t")
 
-fd = open("data1.json", "w")
-fd.write(json_data)
+	# print i,". ",res_val[6]
+	#json_data += '{"name":"'+res_val[6]+'"},'
+	fd.write('{"name":"'+res_val[6]+'"},')
+
+#json_data += ']}'
 fd.close()
 
 # json_data = json.loads(json_data)
