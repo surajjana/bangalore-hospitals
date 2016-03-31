4b2e74dae9c94afd795b05140ca1f3e88cf60938
Suraj Kumar Jana <surajjana2@gmail.com>
2016-03-31 16:28:41 +0530

    new commit

--- test.py
+++ test.py
@@ -1,18 +1,31 @@
+from requests.utils import quote
+from subprocess import call
 import json
 
-count = 0
+fl = open("hospitals_pincode.json","a")
 
-fo = open("data_final.json","r")
-data = fo.read()
+fd = open("data_final.json","r")
+data = fd.read()
 
 json_data = json.loads(data)
 
-for i in range(0,1347):
-	lat = json_data["hospitals"][i]["location"][0]["lat"]
+for i in range(0,1):
+	print i
 
-	#print i+1,". ",str(lat)
+	addr = json_data["hospitals"][i]["address"]
 
-	if str(lat) == '0.0':
-		count = count + 1
+	e_addr = quote(addr)
 
-print "Count : ",count
+	api = 'http://www.getpincode.info/api/pincode?q='+e_addr
+
+	call('curl "'+api+'" > pincode.json',shell=True)
+
+	fo = open("pincode.json","r")
+	data_p = fo.read()
+
+	json_data_p = json.loads(data_p)
+
+	pincode = json_data_p["pincode"]
+	name = json_data["hospitals"][i]["name"]
+
+	fl.write('{"id":'+str(i+1)+',"name":"'+name+'","address":"'+addr+'","pincode":"'+str(pincode)+'"},')
